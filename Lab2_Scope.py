95a5b5a1292654ce68c698192bfebf13a1550930
LS-Song-UC <le.song@ucalgary.ca>
2025-05-08 09:41:27 -0600

    Update Lab2_Scope.py

--- Lab2_Scope.py
+++ Lab2_Scope.py
@@ -11,13 +11,17 @@ for i in range (10):
 
 foo = 100
 bar = foo
-foo + bar
+#print(foo, bar, foo + bar)
 
 # POINT 1
 # What is the value of foo at this point?
+# 100
 # What is the type of foo at this point?
+# int
 # What is the value of bar at this point?
+# 100
 # What is the type of bar at this point?
+# int
 
 spam = foo + bar
 foo += 50
@@ -25,14 +29,20 @@ eggs = foo + bar
 ham = [1, 2, 3]
 baz = ham
 ham.append(bar)
-
+#print(ham, type(bar))
 # POINT 2
 # What is the value of foo at this point?
+# 150
 # What is the value of bar at this point?
+# 100
 # What is the value of spam at this point?
+# 200
 # What is the value of eggs at this point?
+# 250
 # What is the value of ham at this point?
+# [1, 2, 3, 100]
 # What is the value of baz at this point?
+# [1, 2, 3, 100]
 
 eggs = "Python is very flexible!"
 spam = ham
@@ -41,13 +51,18 @@ bar += bar
 foo = eggs
 eggs = bar + ham
 baz.append(bar)
-
+print("foo = ", foo , " with type ", type(foo) , " bar = ",  bar, " with type ", type(bar),  " spam = ",  spam, " with type ", type(spam), " eggs = ",  eggs, " with type ", type(eggs), " ham = ",  ham, " with type ", type(ham)," baz = ",  baz, " with type ", type(baz))
 # POINT 3
 # What is the value of foo at this point?
+# "Python is very flexible!" Type 'Str'
 # What is the value of bar at this point?
+# 200 type int
 # What is the value of spam at this point?
+# [1, 2, 3, 100, 200] type list
 # What is the value of eggs at this point?
+# 300 type int
 # What is the value of ham at this point?
+# 100 type int
 # What is the value of baz at this point?
-
+# [1, 2, 3, 100, 200] type list
 # Print out the types and final values of each variable.
